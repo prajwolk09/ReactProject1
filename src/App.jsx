663fb210e288c1528f471034781967b9ec885016
prajwolK <kprajowl22@tbc.edu.np>
2025-06-15 12:57:06 +0545

    Header Developed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import Search from "./components/Search";
+import { useState } from "react";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
-    <div>App</div>
-  )
-}
+    <main>
+      <div className="pattern" />
+      <img src="./bg.png" alt="Background" />
+      <div className="wrapper" />
+      
+      <header>
+        <img src="./hero-img.png" alt="Hero Banner" />
+        <h1>
+          Find <span className="text-gradient">Movies</span> That You'll Enjoy
+        </h1>
+      </header>
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <h1 className="text-white">{searchTerm}</h1>
+    </main>
+  );
+};
 
-export default App
+export default App;
